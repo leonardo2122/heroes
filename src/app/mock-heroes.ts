7af6afbcf6f09aeff4903d0208b603f663f5ffd7
leonardo2122 <60281178+leonardo2122@users.noreply.github.com>
2020-03-30 10:05:44 -0600

    Update mock-heroes.ts

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
-//import { Hero } from './hero';
+import { Hero } from './hero';
 
-// export const HEROES: Hero[] = [
-//   { id: 11, name: 'Mr. Nice' },
-//   { id: 12, name: 'Narco' },
-//   { id: 13, name: 'Bombasto' },
-//   { id: 14, name: 'Celeritas' },
-//   { id: 15, name: 'Magneta' },
-//   { id: 16, name: 'RubberMan' },
-//   { id: 17, name: 'Dynama' },
-//   { id: 18, name: 'Dr IQ' },
-//   { id: 19, name: 'Magma' },
-//   { id: 20, name: 'Tornado' }
-// ];
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', points: 32 },
+  { id: 12, name: 'Narco', points: 76 },
+  { id: 13, name: 'Bombasto', points: 45 },
+  { id: 14, name: 'Celeritas', points: 23 },
+  { id: 15, name: 'Magneta', points: 34 },
+  { id: 16, name: 'RubberMan', points: 43 },
+  { id: 17, name: 'Dynama', points: 60 },
+  { id: 18, name: 'Dr IQ', points: 54 },
+  { id: 19, name: 'Magma', points: 77 },
+  { id: 20, name: 'Tornado', points: 71 }
+].sort((a, b) => (a.points < b.points ? 1 : -1));
